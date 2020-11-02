4e9dba0b746ba6584c477ddeecc3a28927a2d015
Victor Diniz <victorhenriquediniz@gmail.com>
2020-11-02 11:53:44 -0300

    nothing

---
+++ urna/views/Urna_view.py
@@ -0,0 +1,7 @@
+class UrnaView:
+    def __init__(self):
+        self.texto_codigo = ''
+        self.texto_numero_secao = 0
+        self.texto_turno = ''
+        self.texto_data_homologacao = ''
+        self.texto_data_encerramento = ''
