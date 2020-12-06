97353a53a0fe38e0e0871badea3fa8a6e42bf313
cinthiashiratori <cinthiashiratori@gmail.com>
2020-12-06 16:31:54 -0300

    Conclusao da tela e persistencia do partido

--- views/partido/Partido_consulta_view.py
+++ views/partido/Partido_consulta_view.py
@@ -8,27 +8,40 @@ class Partido_consulta_view():
         self.__partido_cadastro_view = Partido_cadastro_view()
 
     def abrir_tela(self):
-        lista_partidos = Geral_controller().partido_controller.listar_partidos()
+        while True:
+            lista_partidos = Geral_controller().partido_controller.listar_partidos()
 
-        layout = [
-            [sg.Text('PARTIDOS')],
-            [sg.Listbox(values=lista_partidos, select_mode='single', size=(50, 3))],
-            [sg.Button("NOVO"), sg.Button("ALTERAR"), sg.Button("EXCLUIR")]
-        ]
+            layout = [
+                [sg.Text('PARTIDOS')],
+                [sg.Listbox(values=lista_partidos, select_mode='single', size=(50, 3))],
+                [sg.Button("NOVO"), sg.Button("ALTERAR"), sg.Button("EXCLUIR")]
+            ]
 
-        self.__window = sg.Window('PARTIDOS').Layout(layout)
+            self.__window = sg.Window('PARTIDOS').Layout(layout)
 
-        button, values = self.__window.Read()
-        if button == "NOVO":
-            window = self.__partido_cadastro_view.abrir_tela()
-        elif button == "ALTERAR":
-            window = self.__partido_cadastro_view.abrir_tela()
-        elif button == "EXCLUIR":
-            partido_excluir = values[0]
-            partido_split = partido_excluir[0].split(" - ")
-            Geral_controller().partido_controller.excluir_partido(int(partido_split[0]))
-
-        self.__window.close()
+            button, values = self.__window.Read()
+            if button == "NOVO":
+                window = self.__partido_cadastro_view.abrir_tela()
+                self.__window.close()
+            elif button == "ALTERAR":
+                partido_alterar = values[0]
+                if (partido_alterar):
+                    partido_split = partido_alterar[0].split(" - ")
+                    window = self.__partido_cadastro_view.abrir_edicao(int(partido_split[0]))
+                else:
+                    sg.popup("Selecione um registro para alterar.")
+                self.__window.close()
+            elif button == "EXCLUIR":
+                partido_excluir = values[0]
+                if (partido_excluir):
+                    partido_split = partido_excluir[0].split(" - ")
+                    Geral_controller().partido_controller.excluir_partido(int(partido_split[0]))
+                else:
+                    sg.popup("Selecione um registro para excluir.")
+                self.__window.close()
+            elif (button == sg.WIN_CLOSED or button == None):
+                self.__window.close()
+                break
 
     @property
     def partido_cadastro_view(self):
